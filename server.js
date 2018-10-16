74cccfec5af08b32c4aacdfb91b7db0e8802947a
Nguyen Le <ucnguyenle@gmail.com>
2018-10-15 17:10:13 -0700

    add/edit files

---
+++ server.js
@@ -0,0 +1,225 @@
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
+var bodyParser = require('body-parser');
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+
+var PORT = 3000;
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+// Configure middleware
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/nyt", { useNewUrlParser: true });
+
+// route to main page
+app.get('/', (req,res) => {
+  db.Article
+    .find({})
+    .then(function(articles) {
+      res.render('index', {articles});
+      console.log(articles);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+// A GET route for scraping the nytimes US page website
+app.get("/scrape", function(req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.nytimes.com/section/us").then(function(response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    var $ = cheerio.load(response.data);
+
+    // grab the title, link and summary and set it to our database
+    $("div.story-body").each(function(i, element) {
+      // Save an empty result object
+      var result = {};
+
+      // Add the text and href of every link, and save them as properties of the result object
+      result.title = $(this)
+        .find("h2.headline")
+        .text().trim();
+      result.link = $(this)
+        .find("a")
+        .attr("href");
+      result.summary = $(this)
+        .find("p.summary")
+        .text().trim();
+
+
+      // Create a new Article using the `result` object built from scraping
+      db.Article.create(result)
+        .then(function(dbArticle) {
+          // View the added result in the console
+          console.log(dbArticle);
+        })
+        .catch(function(err) {
+          // If an error occurred, send it to the client
+          return res.json(err);
+        });
+    });
+
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.send("Scrape Complete");
+  });
+});
+
+// Route for getting all Articles from the db
+app.get("/articles", function(req, res) {
+  // Grab every document in the Articles collection
+  db.Article.find({})
+    .then(function(dbArticle) {
+      // If we were able to successfully find Articles, send them back to the client
+      res.json(dbArticle);
+    })
+    .catch(function(err) {
+      // If an error occurred, send it to the client
+      res.json(err);
+    });
+});
+
+app.get("/:id", function(req, res) {
+	Article.findById(req.params.id, function(err, data) {
+		res.json(data);
+	})
+})
+
+// this will save or update
+app.get("/save/:id", function(req, res) {
+  db.Article
+  .update({_id: req.params.id},{saved: true})
+  .then(function(result) {
+    res.redirect('/');
+  })
+  .catch(function(err) {
+    err => res.json(err);
+  });
+});
+
+// render savedArticles.handlebars and populate with saved articles
+app.get('/saved', (req, res) => {
+  db.Article
+    .find({})
+    .then(function(result) {
+      res.render('saved', {articles:result});
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+//remove an article from savedArticles
+app.delete('/deleted/:id', function(req,res){
+  db.Article
+    .remove({_id: req.params.id})
+    .then(function(result) {
+      res.json(result);
+    })
+    .catch(function(err) {
+      res.json(err);
+    });
+});
+
+//get route to retrieve all notes for a particlular article
+app.get('/getNotes/:id', function (req,res){
+  db.Article
+    .findOne({_id: req.params.id})
+    .populate('notes')
+    .then(results => res.json(results))
+    .catch(err => res.json(err));
+});
+
+//get route to return a single note to view it
+app.get('/getSingleNote/:id', function (req,res) {
+  db.Note
+  .findOne({_id: req.params.id})
+  .then( result => res.json(result))
+  .catch(err => res.json(err));
+});
+
+//post route to create a new note in the database
+app.post('/createNote', function (req,res){
+  let { title, body, articleId } = req.body;
+  let note = {
+    title,
+    body
+  };
+  db.Note
+    .create(note)
+    .then( result => {
+      db.Article
+        .findOneAndUpdate({_id: articleId}, {$push:{notes: result._id}},{new:true})//saving reference to note in corresponding article
+        .then( data => res.json(result))
+        .catch( err => res.json(err));
+    })
+    .catch(err => res.json(err));
+});
+
+//post route to delete a note
+app.post('/deleteNote', (req,res)=>{
+  let {articleId, noteId} = req.body;
+  db.Note
+    .remove({_id: noteId})
+    .then(result => res.json(result))
+    .catch(err => res.json(err));
+});
+
+// // Route for saving/updating an Article's associated Note
+// app.post("/articles/:id", function(req, res) {
+//   // Create a new note and pass the req.body to the entry
+//   db.Note.create(req.body)
+//     .then(function(dbNote) {
+//       // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+//       // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+//       // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+//       return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+//     })
+//     .then(function(dbArticle) {
+//       // If we were able to successfully update an Article, send it back to the client
+//       res.json(dbArticle);
+//     })
+//     .catch(function(err) {
+//       // If an error occurred, send it to the client
+//       res.json(err);
+//     });
+// });
+
+// Start the server
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
